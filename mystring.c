db4c59220b5be90ac50a4996861963aa8df823f2
stevensll <deltaorb637@gmail.com>
2021-10-07 23:29:49 -0400

    finished; git push
    git add . ; git commit -m finished

--- mystring.c
+++ mystring.c
@@ -10,24 +10,59 @@ int mystrlen( char *s ){
 }
 char * mystrcpy( char *dest, char *source ){
     int i; 
-    for(i = 0; i < n; i++){
+    for(i = 0; source[i]!='\0'; i++){
         dest[i] = source[i];
     }
+    //make sure null value is copied
+    dest[i] = '\0';
     return dest;
 }
+
 char * mystrncpy( char *dest, char *source, int n){
     int i;
-    for(i = 0; i < n; i++){
-        dest[i] = source[i];
+
+    for(i = 0; n > 0 && source[i]; i++){
+        dest[i] == source[i];
+        n--;
     }
+    while(n-- >0){
+        dest[i] = 0;
+    }
+
     return dest;
 }
-char * mystrncat( char *dest, char *source, int n){
-
+char * mystrcat( char *dest, char *source ){
+    char * ret = dest;
+    while(*dest) (dest++);
+    while(*(source)){
+        *dest = *source;
+        dest++;
+        source++;
+    }
+    *dest = 0;
+    return ret;
 }
+
 int mystrcmp( char *s1, char *s2 ){
+    while( *s1 ){
+        if (*s2 == '\0' ||*s1 > *s2 ) return 1;
+        else if (*s1 < *s2) return -1;
+        s1++;
+        s2++;
+    }
+    if (*s1 == *s2) return 0;
+    return -1;
 
 }
 char * mystrchr( char *s, char c ){
-
+    while(*s){
+        if(*s == c){
+            return s;
+        }
+        s++;
+    }
+    if (c=='\0' && *s =='\0'){
+        return s;
+    }
+    return NULL;
 }
--- mystring.h
+++ mystring.h
@@ -1,6 +1,6 @@
 int mystrlen( char *s );
 char * mystrcpy( char *dest, char *source );
 char * mystrncpy( char *dest, char *source, int n);
-char * mystrncat( char *dest, char *source, int n);
+char * mystrcat( char *dest, char *source );
 int mystrcmp( char *s1, char *s2 );
 char * mystrchr( char *s, char c );
